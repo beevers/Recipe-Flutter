49d3e510b9a82154df54eeca261e4ede877848b9
Abolaji Ayeni <ayenibolaji07@gmail.com>
2023-09-11 16:56:49 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import "package:flutter/material.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+
+void main() {
+  runApp(const RecipeApp());
+}
+
+class RecipeApp extends ConsumerWidget {
+  const RecipeApp({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
