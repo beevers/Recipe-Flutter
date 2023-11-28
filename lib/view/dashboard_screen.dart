2b451c34e51bdee7c0a33459b4b10cb5a053004c
Abolaji Ayeni <ayenibolaji07@gmail.com>
2023-11-28 01:38:34 +0100

    removed unused code

--- lib/view/dashboard_screen.dart
+++ lib/view/dashboard_screen.dart
@@ -19,7 +19,7 @@ class _DashboardScreenState extends ConsumerState<DashboardScreen> {
   @override
   void initState() {
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      ref.read(getFoodViewModel).getFood(number: 8, query: "beef");
+      ref.read(getFoodViewModel).getFood(number: 8, query: "apple");
     });
     controller = PageController(
         initialPage: ref.read(screenIndexProvider), keepPage: true);
@@ -29,106 +29,96 @@ class _DashboardScreenState extends ConsumerState<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
     final screenVm = ref.watch(screenProvider);
-    final getFoodVm = ref.watch(getFoodViewModel).getFoodData;
-    return getFoodVm.autoloading
-        ? Center(
-            child: CircularProgressIndicator(
-            color: green,
-          ))
-        : WillPopScope(
-            onWillPop: () async => Future.value(false),
-            child: Scaffold(
-              resizeToAvoidBottomInset: false,
-              body: SafeArea(
-                  child: PageView.builder(
-                      itemCount: screenVm.length,
-                      controller: controller,
-                      onPageChanged: (index) {
-                        if (index >= 2) {
-                          ref.read(screenIndexProvider.notifier).state =
-                              index + 1;
-                        } else {
-                          ref.read(screenIndexProvider.notifier).state = index;
-                        }
-                      },
-                      itemBuilder: (context, index) {
-                        return screenVm[index];
-                      })),
-              bottomNavigationBar: BottomNavigationBar(
-                  type: BottomNavigationBarType.fixed,
-                  currentIndex: ref.watch(screenIndexProvider),
-                  onTap: (index) {
+    return WillPopScope(
+      onWillPop: () async => Future.value(false),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: SafeArea(
+            child: PageView.builder(
+                itemCount: screenVm.length,
+                controller: controller,
+                onPageChanged: (index) {
+                  if (index >= 2) {
+                    ref.read(screenIndexProvider.notifier).state = index + 1;
+                  } else {
                     ref.read(screenIndexProvider.notifier).state = index;
-                    if (index != 2) {
-                      ref.read(screenIndexProvider.notifier).state = index;
-                      controller.animateToPage(
-                        ref.watch(screenIndexProvider) >= 3
-                            ? ref.read(screenIndexProvider.notifier).state - 1
-                            : ref.watch(screenIndexProvider),
-                        duration: const Duration(milliseconds: 50),
-                        curve: Curves.easeInOut,
-                      );
-                    }
-                  },
-                  unselectedItemColor: grey,
-                  selectedItemColor:
-                      ref.watch(screenIndexProvider) == 2 ? grey : green,
-                  items: const [
-                    BottomNavigationBarItem(
-                      icon: Icon(IconlyBold.home),
-                      label: 'Home',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(IconlyBold.chat),
-                      label: 'Chat Bot',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: SizedBox(
-                        height: 20,
-                        width: 20,
-                      ),
-                      label: 'Scan',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(IconlyBold.notification),
-                      label: 'Notification',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(IconlyBold.profile),
-                      label: 'Profile',
-                    ),
-                  ]),
-              floatingActionButton: FloatingActionButton(
-                backgroundColor: green,
-                shape: ShapeBorder.lerp(
-                    const CircleBorder(), const StadiumBorder(), 1.0),
-                onPressed: () async {
-                  await ref.read(scanProvider).scanBarcode(mounted);
-
-                  ref.watch(scanProvider).scanBarCodeRes == "-1"
-                      ? Container()
-                      // ignore: use_build_context_synchronously
-                      : showModalBottomSheet(
-                          context: context,
-                          builder: (context) {
-                            return ModalBottomSheeet(
-                                sheetContent: SizedBox(
-                              width: double.infinity,
-                              child: Text(ref
-                                  .watch(scanProvider)
-                                  .scanBarCodeRes
-                                  .toString()),
-                            ));
-                          });
+                  }
                 },
-                child: Icon(
-                  IconlyBold.scan,
-                  color: white,
-                ),
+                itemBuilder: (context, index) {
+                  return screenVm[index];
+                })),
+        bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            currentIndex: ref.watch(screenIndexProvider),
+            onTap: (index) {
+              ref.read(screenIndexProvider.notifier).state = index;
+              if (index != 2) {
+                ref.read(screenIndexProvider.notifier).state = index;
+                controller.animateToPage(
+                  ref.watch(screenIndexProvider) >= 3
+                      ? ref.read(screenIndexProvider.notifier).state - 1
+                      : ref.watch(screenIndexProvider),
+                  duration: const Duration(milliseconds: 50),
+                  curve: Curves.easeInOut,
+                );
+              }
+            },
+            unselectedItemColor: grey,
+            selectedItemColor:
+                ref.watch(screenIndexProvider) == 2 ? grey : green,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(IconlyBold.home),
+                label: 'Home',
               ),
-              floatingActionButtonLocation:
-                  FloatingActionButtonLocation.centerDocked,
-            ),
-          );
+              BottomNavigationBarItem(
+                icon: Icon(IconlyBold.chat),
+                label: 'Chat Bot',
+              ),
+              BottomNavigationBarItem(
+                icon: SizedBox(
+                  height: 20,
+                  width: 20,
+                ),
+                label: 'Scan',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(IconlyBold.notification),
+                label: 'Notification',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(IconlyBold.profile),
+                label: 'Profile',
+              ),
+            ]),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: green,
+          shape: ShapeBorder.lerp(
+              const CircleBorder(), const StadiumBorder(), 1.0),
+          onPressed: () async {
+            await ref.read(scanProvider).scanBarcode(mounted);
+
+            ref.watch(scanProvider).scanBarCodeRes == "-1"
+                ? Container()
+                // ignore: use_build_context_synchronously
+                : showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return ModalBottomSheeet(
+                          sheetContent: SizedBox(
+                        width: double.infinity,
+                        child: Text(
+                            ref.watch(scanProvider).scanBarCodeRes.toString()),
+                      ));
+                    });
+          },
+          child: Icon(
+            IconlyBold.scan,
+            color: white,
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      ),
+    );
   }
 }
