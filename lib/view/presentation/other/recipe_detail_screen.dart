ad59c8f0fb905f910c1480204ca0e3005882636a
Abolaji Ayeni <ayenibolaji07@gmail.com>
2023-12-14 00:07:26 +0100

    removed unused code(modal sheet)

--- lib/view/presentation/other/recipe_detail_screen.dart
+++ lib/view/presentation/other/recipe_detail_screen.dart
@@ -31,10 +31,13 @@ class _RecipeDetailScreenState extends ConsumerState<RecipeDetailScreen> {
               child: Stack(
                 children: [
                   Container(
-                    color: red,
-                    width: double.infinity,
-                    height: 375.h,
-                  ),
+                      color: red,
+                      width: double.infinity,
+                      height: 375.h,
+                      child: Image.network(
+                        "https://spoonacular.com/recipeImages/632573-312x231.jpg",
+                        fit: BoxFit.cover,
+                      )),
                   Align(
                     alignment: const Alignment(-0.89, -0.9),
                     child: Stack(
@@ -83,133 +86,3 @@ class _RecipeDetailScreenState extends ConsumerState<RecipeDetailScreen> {
     );
   }
 }
-
-// class ModalContent extends ConsumerWidget {
-//   const ModalContent({super.key});
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     return Padding(
-//       padding: EdgeInsets.only(left: 24.w, right: 24.w),
-//       child: SizedBox(
-//         height: 400,
-//         child: Column(
-//           children: [
-//             Padding(
-//               padding: EdgeInsets.only(top: 16.h),
-//               child: Container(
-//                 decoration: BoxDecoration(
-//                     color: grey, borderRadius: BorderRadius.circular(5.r)),
-//                 width: 40.w,
-//                 height: 5.h,
-//               ),
-//             ),
-//             HelpSpace.h(23),
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.start,
-//               children: [
-//                 Text(
-//                   "Cacao Maca Walnut Milk",
-//                   style: RecipeText.medium(color: const Color(0xff3d5481)),
-//                 ),
-//               ],
-//             ),
-//             HelpSpace.h(8),
-//             Row(
-//               children: [
-//                 Text(
-//                   "Food",
-//                   style: RecipeText.small(),
-//                 ),
-//                 HelpSpace.w(2),
-//                 Image.asset(
-//                   "assets/images/onboarding/big_dot.png",
-//                   scale: 1.5,
-//                 ),
-//                 Text(
-//                   "  60 mins",
-//                   style: RecipeText.small(),
-//                 ),
-//               ],
-//             ),
-//             HelpSpace.h(16),
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//               children: [
-//                 Row(
-//                   children: [
-//                     const ProfilePictureCard(isRound: true),
-//                     HelpSpace.w(8),
-//                     Text("Elena Shwelby", style: RecipeText.small(color: dBlue))
-//                   ],
-//                 ),
-//                 Row(
-//                   children: [
-//                     const ProfilePictureCard(isRound: true),
-//                     HelpSpace.w(8),
-//                     Text("273 Likes", style: RecipeText.small(color: dBlue))
-//                   ],
-//                 ),
-//               ],
-//             ),
-//             HelpSpace.h(16),
-//             Divider(
-//               color: lightGrey,
-//               thickness: 1,
-//             ),
-//             HelpSpace.h(16),
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.start,
-//               children: [
-//                 Text(
-//                   "Description",
-//                   style: RecipeText.small(color: dBlue),
-//                 ),
-//               ],
-//             ),
-//             HelpSpace.h(8),
-//             const Row(
-//               mainAxisAlignment: MainAxisAlignment.start,
-//               children: [
-//                 SizedBox(
-//                   width: 300,
-//                   height: 75,
-//                   child: Text(
-//                     "Your recipe has been uploaded, you can see it on your profile. Your recipe has been uploaded, you can see it on your",
-//                   ),
-//                 ),
-//               ],
-//             ),
-//             Divider(
-//               color: lightGrey,
-//               thickness: 1,
-//             ),
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.start,
-//               children: [
-//                 Text("Ingredients", style: RecipeText.small(color: dBlue)),
-//               ],
-//             ),
-//             HelpSpace.h(16),
-//             Column(
-//               children: List.generate(
-//                 70,
-//                 (index) => Row(
-//                   children: [
-//                     const TickCard(
-//                       isValidated: true,
-//                     ),
-//                     HelpSpace.w(5),
-//                     Text(
-//                       "4 eggs",
-//                       style: RecipeText.small(color: dBlue),
-//                     ),
-//                   ],
-//                 ),
-//               ),
-//             )
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
