3d87d1c436e350ff0349ca38156cc2985f9d75b9
Abolaji Ayeni <ayenibolaji07@gmail.com>
2023-12-13 21:11:52 +0100

    fixed bug on loading error on homescreen

--- lib/view/presentation/home/home_screen.dart
+++ lib/view/presentation/home/home_screen.dart
@@ -45,196 +45,217 @@ class _HomeScreenState extends ConsumerState<HomeScreen>
   @override
   Widget build(BuildContext context) {
     final foodVm = ref.watch(getFoodViewModel).getFoodData.data;
-    return foodVm == null
-        ? Center(
-            child: CircularProgressIndicator(
-              color: green,
-            ),
-          )
-        : Padding(
-            padding: const EdgeInsets.only(top: 16),
-            child: Center(
-              child: Column(
-                children: [
-                  AppFormField(
-                    readOnly: true,
-                    onTap: () {
-                      Get.to(() => const SearchScreen());
-                    },
-                    isObscure: false,
-                    validator: (v) {
-                      return null;
-                    },
-                    controller: TextEditingController(),
-                    isIcon: true,
-                    title: "Search",
-                    prefixIcon: IconlyLight.search,
-                  ),
-                  HelpSpace.h(16),
-                  Padding(
-                    padding: EdgeInsets.only(left: 20.w),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Category",
-                          style:
-                              RecipeText.medium(color: const Color(0xff3d5481)),
-                        ),
-                      ],
+    final drinkVm = ref.watch(getDrinkViewModel).getDrinkData.data;
+    return Padding(
+        padding: const EdgeInsets.only(top: 16),
+        child: Center(
+          child: Column(
+            children: [
+              AppFormField(
+                readOnly: true,
+                onTap: () {
+                  Get.to(() => const SearchScreen());
+                },
+                isObscure: false,
+                validator: (v) {
+                  return null;
+                },
+                controller: TextEditingController(),
+                isIcon: true,
+                title: "Search",
+                prefixIcon: IconlyLight.search,
+              ),
+              HelpSpace.h(16),
+              Padding(
+                padding: EdgeInsets.only(left: 20.w),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Category",
+                      style: RecipeText.medium(color: const Color(0xff3d5481)),
                     ),
-                  ),
-                  HelpSpace.h(16),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 10.w),
-                      child: Row(
-                        children: List.generate(
+                  ],
+                ),
+              ),
+              HelpSpace.h(16),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Padding(
+                  padding: EdgeInsets.only(left: 10.w),
+                  child: Row(
+                    children: List.generate(
+                        ref.watch(viewDrinksProvider)
+                            ? drinkOption.length
+                            : foodOption.length, (index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(right: 10),
+                        child: FilterCard(
+                          selectedIndex: ref.watch(viewDrinksProvider)
+                              ? ref.watch(drinkSelectedIndexProvider)
+                              : ref.watch(selectedIndexProvider),
+                          function: () {
                             ref.watch(viewDrinksProvider)
-                                ? drinkOption.length
-                                : foodOption.length, (index) {
-                          return Padding(
-                            padding: const EdgeInsets.only(right: 10),
-                            child: FilterCard(
-                              selectedIndex: ref.watch(viewDrinksProvider)
-                                  ? ref.watch(drinkSelectedIndexProvider)
-                                  : ref.watch(selectedIndexProvider),
-                              function: () {
-                                ref.watch(viewDrinksProvider)
-                                    ? ref
-                                        .read(
-                                            drinkSelectedIndexProvider.notifier)
-                                        .state = index
-                                    : ref
-                                        .read(selectedIndexProvider.notifier)
-                                        .state = index;
+                                ? ref
+                                    .read(drinkSelectedIndexProvider.notifier)
+                                    .state = index
+                                : ref
+                                    .read(selectedIndexProvider.notifier)
+                                    .state = index;
 
-                                //
-                                ref.watch(viewDrinksProvider)
-                                    ? ref.read(optionProvider.notifier).state =
-                                        drinkOption[index]
-                                    : ref.read(optionProvider.notifier).state =
-                                        foodOption[index];
+                            //
+                            ref.watch(viewDrinksProvider)
+                                ? ref.read(optionProvider.notifier).state =
+                                    drinkOption[index]
+                                : ref.read(optionProvider.notifier).state =
+                                    foodOption[index];
 
-                                // //
+                            // This method below is used to get data from the api
 
-                                // ref.watch(viewDrinksProvider)
-                                //     ? ref.read(getDrinkViewModel).getDrink(
-                                //         number: 20,
-                                //         query: drinkOption[index].toLowerCase())
-                                //     : ref.read(getFoodViewModel).getFood(
-                                //         number: 20,
-                                //         query: foodOption[index].toLowerCase());
-                              },
-                              index: index,
-                            ),
-                          );
-                        }),
-                      ),
-                    ),
+                            // ref.watch(viewDrinksProvider)
+                            //     ? ref.read(getDrinkViewModel).getDrink(
+                            //         number: 20,
+                            //         query: drinkOption[index].toLowerCase())
+                            //     : ref.read(getFoodViewModel).getFood(
+                            //         number: 20,
+                            //         query: foodOption[index].toLowerCase());
+                            //This method above is used to get data from the api
+                          },
+                          index: index,
+                        ),
+                      );
+                    }),
                   ),
-                  HelpSpace.h(17),
-                  const Divider(
-                    color: Color(0xffF4F5F7),
-                    thickness: 8,
-                  ),
-                  HelpSpace.h(5),
-                  TabBar(
-                    onTap: (value) {
-                      ref.read(viewDrinksProvider.notifier).state = value == 1;
-                      ref.watch(viewDrinksProvider)
-                          ? ref.read(optionProvider.notifier).state =
-                              drinkOption[ref.watch(drinkSelectedIndexProvider)]
-                          : ref.read(optionProvider.notifier).state =
-                              foodOption[ref.watch(selectedIndexProvider)];
-                    },
-                    labelColor: const Color(0xff3E5481),
-                    unselectedLabelColor: grey,
-                    indicatorWeight: 3,
-                    indicatorSize: TabBarIndicatorSize.tab,
-                    indicatorColor: green,
-                    controller: controller,
-                    tabs: const [
-                      Tab(text: "Foods"),
-                      Tab(text: "Drinks"),
-                    ],
-                  ),
-                  HelpSpace.h(8),
-                  Expanded(
-                    child: TabBarView(
-                      controller: controller,
-                      children: [
-                        // // Content for Tab 1
-                        ref.watch(getFoodViewModel).getFoodData.loading
-                            ? const LoadingIndicator()
-                            : GridView.builder(
-                                gridDelegate:
-                                    const SliverGridDelegateWithFixedCrossAxisCount(
-                                  crossAxisCount:
-                                      2, // number of items in each row
-                                  mainAxisSpacing: 12.5, // spacing between rows
-                                  crossAxisSpacing:
-                                      12.5, // spacing between columns
-                                  childAspectRatio:
-                                      0.67, // Adjust this value to control aspect ratio
-                                ),
-                                padding: const EdgeInsets.all(
-                                    8.0), // padding around the grid
-                                itemCount: foodVm.searchResults![0].results!
-                                    .length, // total number of items
-                                itemBuilder: (context, index) {
-                                  extractedMinutes = AppServices.extractMinutes(
-                                      foodVm.searchResults![0].results![index]
-                                          .content
-                                          .toString());
-                                  return foodVm
-                                          .searchResults![0].results!.isEmpty
-                                      ? const Center(child: Text("Empty"))
-                                      : FoodCardTemplate(
-                                          index: index,
-                                          onTap: () {
-                                            Get.to(() =>
-                                                const RecipeDetailScreen());
-                                          },
-                                        );
-                                },
-                              ),
-
-                        // Content for Tab 2
-                        ref.watch(getDrinkViewModel).getDrinkData.loading
-                            ? const LoadingIndicator()
-                            : GridView.builder(
-                                gridDelegate:
-                                    const SliverGridDelegateWithFixedCrossAxisCount(
-                                  crossAxisCount:
-                                      2, // number of items in each row
-                                  mainAxisSpacing: 12.5, // spacing between rows
-                                  crossAxisSpacing:
-                                      12.5, // spacing between columns
-                                  childAspectRatio:
-                                      0.67, // Adjust this value to control aspect ratio
-                                ),
-                                padding: const EdgeInsets.all(
-                                    8.0), // padding around the grid
-                                itemCount: ref
-                                    .watch(getDrinkViewModel)
-                                    .getDrinkData
-                                    .data
-                                    ?.recommendedWines
-                                    ?.length, // total number of items
-                                itemBuilder: (context, index) {
-                                  return DrinkCardTemplate(
-                                    index: index,
-                                    onTap: null,
-                                  );
-                                },
-                              ),
-                      ],
-                    ),
-                  ),
+                ),
+              ),
+              HelpSpace.h(17),
+              const Divider(
+                color: Color(0xffF4F5F7),
+                thickness: 8,
+              ),
+              HelpSpace.h(5),
+              TabBar(
+                onTap: (value) {
+                  ref.read(viewDrinksProvider.notifier).state = value == 1;
+                  ref.watch(viewDrinksProvider)
+                      ? ref.read(optionProvider.notifier).state =
+                          drinkOption[ref.watch(drinkSelectedIndexProvider)]
+                      : ref.read(optionProvider.notifier).state =
+                          foodOption[ref.watch(selectedIndexProvider)];
+                },
+                labelColor: const Color(0xff3E5481),
+                unselectedLabelColor: grey,
+                indicatorWeight: 3,
+                indicatorSize: TabBarIndicatorSize.tab,
+                indicatorColor: green,
+                controller: controller,
+                tabs: const [
+                  Tab(text: "Foods"),
+                  Tab(text: "Drinks"),
                 ],
               ),
-            ));
+              HelpSpace.h(8),
+              Expanded(
+                child: TabBarView(
+                  controller: controller,
+                  children: [
+                    // // Content for Tab 1
+                    ref.watch(getFoodViewModel).getFoodData.loading ||
+                            foodVm == null
+                        ? GridView.builder(
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2, // number of items in each row
+                              mainAxisSpacing: 12.5, // spacing between rows
+                              crossAxisSpacing: 12.5, // spacing between columns
+                              childAspectRatio:
+                                  0.67, // Adjust this value to control aspect ratio
+                            ),
+                            padding: const EdgeInsets.all(
+                                8.0), // padding around the grid
+                            itemCount: 4, // total number of items
+                            itemBuilder: (context, index) {
+                              return const LoadingIndicator();
+                            },
+                          )
+                        : GridView.builder(
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2, // number of items in each row
+                              mainAxisSpacing: 12.5, // spacing between rows
+                              crossAxisSpacing: 12.5, // spacing between columns
+                              childAspectRatio:
+                                  0.67, // Adjust this value to control aspect ratio
+                            ),
+                            padding: const EdgeInsets.all(
+                                8.0), // padding around the grid
+                            itemCount: foodVm.searchResults?[0].results!
+                                .length, // total number of items
+                            itemBuilder: (context, index) {
+                              extractedMinutes = AppServices.extractMinutes(
+                                  foodVm
+                                      .searchResults![0].results![index].content
+                                      .toString());
+                              return foodVm.searchResults![0].results!.isEmpty
+                                  ? const Center(child: Text("Empty"))
+                                  : FoodCardTemplate(
+                                      index: index,
+                                      onTap: () {
+                                        Get.to(
+                                            () => const RecipeDetailScreen());
+                                      },
+                                    );
+                            },
+                          ),
+
+                    // Content for Tab 2
+                    ref.watch(getDrinkViewModel).getDrinkData.loading ||
+                            drinkVm == null
+                        ? GridView.builder(
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2, // number of items in each row
+                              mainAxisSpacing: 12.5, // spacing between rows
+                              crossAxisSpacing: 12.5, // spacing between columns
+                              childAspectRatio:
+                                  0.67, // Adjust this value to control aspect ratio
+                            ),
+                            padding: const EdgeInsets.all(
+                                8.0), // padding around the grid
+                            itemCount: 4, // total number of items
+                            itemBuilder: (context, index) {
+                              return const LoadingIndicator();
+                            },
+                          )
+                        : GridView.builder(
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2, // number of items in each row
+                              mainAxisSpacing: 12.5, // spacing between rows
+                              crossAxisSpacing: 12.5, // spacing between columns
+                              childAspectRatio:
+                                  0.67, // Adjust this value to control aspect ratio
+                            ),
+                            padding: const EdgeInsets.all(
+                                8.0), // padding around the grid
+                            itemCount: ref
+                                .watch(getDrinkViewModel)
+                                .getDrinkData
+                                .data
+                                ?.recommendedWines
+                                ?.length, // total number of items
+                            itemBuilder: (context, index) {
+                              return DrinkCardTemplate(
+                                index: index,
+                                onTap: null,
+                              );
+                            },
+                          ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
